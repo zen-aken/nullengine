aa82ce74a925e6162eec09d9f00fe7b35e08e57f
zen-aken <arslanahmet4772@gmail.com>
2026-08-18 23:13:20 +0200

    checkpoint

---
+++ src/include/body/RigidBody2d.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <vec2d.hpp>
+
+struct body2d
+{
+    Vec2 position;
+    Vec2 velocity;
+    Vec2 mass;
+    Vec2 force;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,29 @@
 #include <iostream>
-#include "vector/vec2.hpp"
+#include "vector/vec2d.hpp"
+
+Vec2 direction{0.0f, 0.0f};
+Vec2 velocity{0.0f, 0.0f};
+Vec2 acceleration{0.0f, 0.0f};
+Vec2 gravity{0.0f, -9.81f};
+
+float delta = 1.0f / 60.0f;
+float speed = 20.0f;
 
 int main()
 {
-    Vec2 position{2, 4};
-    Vec2 target{10, 5};
-    Vec2 direction = (target - position).normalize();
+    Vec2 position{2.0f, 4.0f};
+    Vec2 target{10.0f, 5.0f};
 
-    Vec2 gravity{0, -9.81};
-    int speed = 300;
+    direction = (target - position).normalize();
+    velocity = direction * speed;
+    acceleration = acceleration + gravity;
 
     int count = 0;
-    float delta = 1.0f / 60.0f;
     printf("position; x: %f, y: %f\n", position.x, position.y);
     while (count <= 60)
     {
-        position = position + direction * gravity * speed * delta;
-
+        velocity = velocity + acceleration * delta;
+        position = position + velocity * delta;
         printf("position; x: %f, y: %f\n", position.x, position.y);
         count++;
     }
